cea0f65514acc9eee9a0e22387234f6ca22aeaa7
이화중 <hj_tl22@live.co.kr>
2016-04-24 12:01:12 +0900

    Selectable socket type (TCP/UDP) - client

--- CalculatorAgent/src/calculator/client/main.py
+++ CalculatorAgent/src/calculator/client/main.py
@@ -9,26 +9,20 @@ if __name__ == '__main__':
     from socket import *        # 소켓 생성을 위한 모듈
     serverName = '192.168.0.10' # 서버의 IP 주소
     serverPort = 12000          # 서버의 포트 번호
+    socketType = SOCK_STREAM    # 소켓 타입 (SOCK_DGRAM: UDP, SOCK_STREAM: TCP)
+    bufferSize = 1024           # 메시지 버퍼 크기
     
-    clientSocket = socket(AF_INET, SOCK_DGRAM)                      # 클라이언트 측에 IPv4(AF_INET)와 UDP(SOCK_DGRAM)를 사용하는 소켓 생성
-    message = input('Input lowercase sentence:')                    # 사용자 입력
-    clientSocket.sendto(message.encode(),(serverName, serverPort))  # 클라이언트 소켓을 통해 서버로 메시지를 보내고 수신을 기다림
+    clientSocket = socket(AF_INET, socketType)      # 클라이언트 측에 IPv4(AF_INET) 소켓 생성
+    message = input('Input lowercase sentence:')    # 사용자 입력
     
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)    # 받은 데이터 할당
-    print('From Server:', modifiedMessage)                          # 받은 메시지 출력
-    clientSocket.close()                                            # 소켓을 닫음
-
-
-### TCP ###
-#     from socket import *        # 소켓 생성을 위한 모듈
-#     serverName = '192.168.0.10' # 서버의 IP 주소
-#     serverPort = 12000          # 서버의 포트 번호
-#       
-#     clientSocket = socket(AF_INET, SOCK_STREAM)     # 클라이언트 측에 IPv4(AF_INET)와 TCP(SOCK_STREAM)를 사용하는 소켓 생성
-#     clientSocket.connect((serverName,serverPort))   # 핸드 셰이크 및 TCP 연결 설정
-#     sentence = input('Input lowercase sentence:')   # 사용자 입력
-#     clientSocket.send(sentence.encode())            # 클라이언트 소켓을 통해 메시지를 서버로 보내고 수신을 기다림
-#       
-#     modifiedSentence = clientSocket.recv(1024)      # 받은 데이터 할당
-#     print('From Server:', modifiedSentence)         # 받은 메시지 출력
-#     clientSocket.close()                            # 소켓을 닫음
+    if socketType == SOCK_DGRAM:                                            # UDP(SOCK_DGRAM)
+        clientSocket.sendto(message.encode(),(serverName, serverPort))      # 클라이언트 소켓을 통해 서버로 메시지를 보내고 수신을 기다림
+        modifiedMessage, serverAddress = clientSocket.recvfrom(bufferSize)  # 받은 데이터 할당
+        
+    elif socketType == SOCK_STREAM:                     # TCP(SOCK_STREAM)
+        clientSocket.connect((serverName,serverPort))   # 핸드 셰이크 및 TCP 연결 설정
+        clientSocket.send(message.encode())             # 클라이언트 소켓을 통해 메시지를 서버로 보내고 수신을 기다림
+        modifiedMessage = clientSocket.recv(bufferSize) # 받은 데이터 할당
+    
+    print('From Server:', modifiedMessage)              # 받은 메시지 출력
+    clientSocket.close()                                # 소켓을 닫음
