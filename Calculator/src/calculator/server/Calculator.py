a2a9b358d501653f4ca09d3b8856e1817217a1ac
이화중 <hj_tl22@live.co.kr>
2016-04-25 22:59:35 +0900

    Implemented calculator on server

---
+++ Calculator/src/calculator/server/Calculator.py
@@ -0,0 +1,72 @@
+'''
+Created on 2016. 4. 24.
+
+@author: Hwajung Lee
+'''
+
+class Calculator(object):
+    '''
+    Simple Arithmetic Calculator
+    '''
+    memory = 0
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.memory = 0
+    
+    def calculate(self, formula):
+        form = formula.split('=')[0]
+        self.memory = eval(form)
+        return self.memory
+        
+    def plus(self, num1, num2 = None):
+        if num2 is None:
+            self.memory = num1 + num2
+        else:
+            self.memory += num1
+        return self.memory
+    
+    def sum(self, *nums):
+        self.memory = 0
+        for num in nums:
+            self.memory += num
+        return self.memory
+    
+    def minus(self, num1, num2 = None):
+        if num2 is None:
+            self.memory -= num1
+        else:
+            self.memory = num1 - num2
+        return self.memory
+    
+    def multifly(self, num1, num2 = None):
+        if num2 is None:
+            self.memory *= num1
+        else:
+            self.memory = num1 * num2
+        return self.memory
+    
+    def devide(self, num1, num2 = None):
+        if num2 is None:
+            self.memory /= num1
+        else:
+            self.memory = num1 / num2
+        return self.memory
+    
+    def mod(self, num1, num2 = None):
+        if num2 is None:
+            self.memory %= num1
+        else:
+            self.memory = num1 % num2
+        return self.memory
+    
+    def getMemory(self):
+        return self.memory
+    
+    def setMemory(self, value):
+        self.memory = value
+    
+    def resetMemory(self):
+        self.memory = 0
--- Calculator/src/calculator/server/main.py
+++ Calculator/src/calculator/server/main.py
@@ -4,31 +4,44 @@ Created on 2016. 4. 16.
 @author: Hwajung Lee
 '''
 
+from calculator.server.Calculator import Calculator
+
 def proceed(message, address):
-    modifiedMessage = message.upper()
-    print (address, ": ", modifiedMessage)
-    return modifiedMessage
+    decodedMsg = message.decode()
+    form = decodedMsg.split('=')
+    print ("Received from", address, ":\t", decodedMsg)
+    cal = Calculator()
+    try:
+        result = form[0] + "= "
+        result += str(cal.calculate(form[0]))
+    except Exception as e:
+        result = "Syntax Error"
+        print ("Error:", e)
+    print ("Send to", address, ":\t\t", result)
+    return result.encode()
 
 if __name__ == '__main__':
-    from socket import *        # 소켓 생성을 위한 모듈
-    serverPort = 12000          # 서버의 포트 번호
-    socketType = SOCK_STREAM    # 소켓 타입 (SOCK_DGRAM: UDP, SOCK_STREAM: TCP)
-    bufferSize = 1024           # 메시지 버퍼 크기
+    import socket
+    serverPort = 12004              # 서버의 포트 번호
+    socketType = socket.SOCK_STREAM # 소켓 타입 (SOCK_DGRAM: UDP, SOCK_STREAM: TCP)
+    bufferSize = 1024               # 메시지 버퍼 크기
     
-    serverSocket = socket(AF_INET, socketType)  # 서버 측에 IPv4(AF_INET)와 UDP(SOCK_DGRAM)를 사용하는 소켓 생성
-    serverSocket.bind(('', serverPort))         # 서버 소켓에 포트 번호 할당
-    if socketType == SOCK_STREAM:               # TCP(SOCK_STREAM)
-        serverSocket.listen(1)                  # 연결 요청 듣기 (최대 연결수는 1)
+    serverSocket = socket.socket(socket.AF_INET, socketType)    # 서버 측에 IPv4(AF_INET)와 UDP(SOCK_DGRAM)를 사용하는 소켓 생성
+    serverSocket.bind(('', serverPort))                         # 서버 소켓에 포트 번호 할당
+    if socketType == socket.SOCK_STREAM:                        # TCP(SOCK_STREAM)
+        serverSocket.listen(1)                                  # 연결 요청 듣기 (최대 연결수는 1)
     print ("The server is ready to receive")
     
-    if socketType == SOCK_DGRAM:                                                # UDP(SOCK_DGRAM)
+    if socketType == socket.SOCK_DGRAM:                                         # UDP(SOCK_DGRAM)
         while True:
             message, clientAddress = serverSocket.recvfrom(bufferSize)          # 서버 소켓을 통해 클라이언트로부터 메시지를 받음
             serverSocket.sendto(proceed(message, clientAddress), clientAddress) # 결과를 소켓으로 보냄
         
-    elif socketType == SOCK_STREAM:                                 # TCP(SOCK_STREAM)
+    elif socketType == socket.SOCK_STREAM:                          # TCP(SOCK_STREAM)
         while True:
             connectionSocket, clientAddress = serverSocket.accept() # 연결된 클라이언트에 특정된 서버 사이에, 새로운 소켓 생성을 생성하고 TCP 연결
             message = connectionSocket.recv(bufferSize)             # 소켓을 통해 클라이언트로부터 메시지를 받음
             connectionSocket.send(proceed(message, clientAddress))  # 결과를 소켓으로 보냄
             connectionSocket.close()                                # 소켓을 닫음
+    
+    serverSocket.close()
